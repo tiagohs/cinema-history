a9dbc93420751da276b944873271bc1c62387dac
Tiago Silva <tiago.silva@ingresso.com>
2020-09-29 15:51:56 -0300

    add content

--- android/entities/src/main/java/com/tiagohs/entities/enums/LocalFiles.kt
+++ android/entities/src/main/java/com/tiagohs/entities/enums/LocalFiles.kt
@@ -40,6 +40,7 @@ enum class LocalFiles(
 
     HISTORY_SUMARIO_1("/hmt_sumario_1", "local/history_sumarios/hmt_sumarios_1.json"),
     HISTORY_SUMARIO_2("/hmt_sumario_2", "local/history_sumarios/hmt_sumarios_2.json"),
+    HISTORY_SUMARIO_3("/hmt_sumario_3", "local/history_sumarios/hmt_sumarios_3.json"),
 
     // Main Topic 1985 to 1929
     MAIN_1_PAGE_1("/main_1/page_1", "local/pages/main_1/page_1.json"),
@@ -61,8 +62,20 @@ enum class LocalFiles(
     MAIN_2_PAGE_6("/main_2/page_6", "local/pages/main_2/page_6.json"),
     MAIN_2_PAGE_7("/main_2/page_7", "local/pages/main_2/page_7.json"),
     MAIN_2_PAGE_8("/main_2/page_8", "local/pages/main_2/page_8.json"),
-    MAIN_2_PAGE_9("/main_2/page_9", "local/pages/main_2/page_9.json");
+    MAIN_2_PAGE_9("/main_2/page_9", "local/pages/main_2/page_9.json"),
 
+    // Main Topic 1940 a 1959
+    MAIN_3_PAGE_1("/main_3/page_1", "local/pages/main_3/page_1.json"),
+    MAIN_3_PAGE_2("/main_3/page_2", "local/pages/main_3/page_2.json"),
+    MAIN_3_PAGE_3("/main_3/page_3", "local/pages/main_3/page_3.json"),
+    MAIN_3_PAGE_4("/main_3/page_4", "local/pages/main_3/page_4.json"),
+    MAIN_3_PAGE_5("/main_3/page_5", "local/pages/main_3/page_5.json"),
+    MAIN_3_PAGE_6("/main_3/page_6", "local/pages/main_3/page_6.json"),
+    MAIN_3_PAGE_7("/main_3/page_7", "local/pages/main_3/page_7.json"),
+    MAIN_3_PAGE_8("/main_3/page_8", "local/pages/main_3/page_8.json"),
+    MAIN_3_PAGE_9("/main_3/page_9", "local/pages/main_3/page_9.json"),
+    MAIN_3_PAGE_10("/main_3/page_10", "local/pages/main_3/page_10.json"),
+    MAIN_3_PAGE_11("/main_3/page_11", "local/pages/main_3/page_11.json");
 
     fun isValid(request: Request): Boolean = request.url.toUri().path.contains(path, true)
 }
